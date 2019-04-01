7b2f7868757655d9645c5bf5faa1e38798de9197
alex broley <alexbroley@alexs-MacBook-Pro.local>
2019-03-31 18:36:02 -0700

    Added unit tests for all SLLNode methods

--- unittests.py
+++ unittests.py
@@ -2,11 +2,27 @@ import unittest
 
 from sll.sll import SLLNode
 
-class TestDataStructures(unittest.TestCase):
+class TestSLLMethods(unittest.TestCase):
     def test_SLLNode_get_data(self):
         node = SLLNode(0)
         self.assertEqual(node.get_data(), 0)
 
+    def test_SLLNode_set_data(self):
+        node = SLLNode(0)
+        node.set_data(1)
+        self.assertEqual(node.get_data(), 1)
+
+    def test_SLLNode_get_next(self):
+        node0 = SLLNode(0)
+        node1 = SLLNode(1)
+        node0.next = node1
+        self.assertEqual(node0.get_next(), node1)
+
+    def test_SLLNode_set_next(self):
+        node0 = SLLNode(0)
+        node1 = SLLNode(1)
+        node0.set_next(node1)
+        self.assertEqual(node0.get_next(), node1)
 
 if __name__ == '__main__':
     unittest.main()
