6f954a13b9c7e88be91813a82786cee954a6d099
alex broley <alexbroley@alexs-MacBook-Pro.local>
2019-03-31 21:20:53 -0700

    Implemented single linked list methods; added unit tests for each single linked list method

--- sll/sll.py
+++ sll/sll.py
@@ -25,4 +25,80 @@ class SLLNode(object):
     def set_next(self, node):
         self.next = node
 
-    
+
+"""
+Single linked list attributes and methods
+head
+next
+add_top
+add_bot
+remove
+contains
+size
+"""
+
+class SLL(object):
+    def __init__(self):
+        """ initialize an emtpy single linked list """
+        self.head = None
+        self.next = None
+
+    def add_top(self, data):
+        """ add a node containing data to top of list """
+        node = SLLNode(data)
+        curr = self.head
+        self.head = node
+        if curr:
+            node.set_next(curr)
+
+    def add_bot(self, data):
+        """ add a node containing data to bottom of list """
+        node = SLLNode(data)
+        curr = self.head
+        prev = None
+        while curr:
+            prev = curr
+            curr = curr.get_next()
+        if prev:
+            prev.set_next(node)
+        else:
+            self.head = node
+
+    def size(self):
+        """ return the number of nodes in list """
+        cnt = 0
+        curr = self.head
+        while curr:
+            cnt += 1
+            curr = curr.get_next()
+        return cnt
+
+    def remove(self, node):
+        """ delete node from list """
+        next = node.get_next()
+        if next:
+            value = next.get_data()
+            node.set_data(value)
+            node.set_next(next.get_next())
+        else:
+            curr = self.head
+            prev = None
+            while curr.get_next():
+                prev = curr
+                curr = curr.get_next()
+            if prev:
+                prev.set_next(None)
+            else:
+                self.head = None
+
+    def contains(self, data):
+        """ returns True if there exists a node containing data """
+        found = False
+        curr = self.head
+        while curr and not found:
+            value = curr.get_data()
+            if value == data:
+                found = True
+            else:
+                curr = curr.get_next()
+        return found
--- unittests.py
+++ unittests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from sll.sll import SLLNode
+from sll.sll import SLL
 
 class TestSLLMethods(unittest.TestCase):
     def test_SLLNode_get_data(self):
@@ -23,6 +24,64 @@ class TestSLLMethods(unittest.TestCase):
         node1 = SLLNode(1)
         node0.set_next(node1)
         self.assertEqual(node0.get_next(), node1)
+
+    def test_SLL_add_top(self):
+        sll = SLL()
+        sll.add_top(0)
+        sll.add_top(1)
+        self.assertEqual(sll.head.get_data(), 1)
+        self.assertEqual(sll.head.get_next().get_data(), 0)
+
+    def test_SLL_add_bot(self):
+        sll = SLL()
+        sll.add_bot(0)
+        sll.add_bot(1)
+        self.assertEqual(sll.head.get_data(), 0)
+        self.assertEqual(sll.head.get_next().get_data(), 1)
+
+    def test_SLL_size(self):
+        sll = SLL()
+        self.assertEqual(sll.size(), 0)
+        sll.add_top(0)
+        self.assertEqual(sll.size(), 1)
+
+    def test_SLL_contains(self):
+        sll = SLL()
+        self.assertTrue(not sll.contains(0))
+        sll.add_top(0)
+        self.assertTrue(sll.contains(0))
+
+    def test_SLL_remove_top(self):
+        sll = SLL()
+        sll.add_top(0)
+        sll.add_top(1)
+        sll.add_top(2)
+        node = sll.head
+        sll.remove(node)
+        self.assertEqual(sll.head.get_data(), 1)
+        self.assertEqual(sll.size(), 2)
+
+    def test_SLL_remove_mid(self):
+        sll = SLL()
+        sll.add_top(0)
+        sll.add_top(1)
+        sll.add_top(2)
+        node = sll.head.get_next()
+        sll.remove(node)
+        self.assertEqual(sll.head.get_data(), 2)
+        self.assertEqual(sll.head.get_next().get_data(), 0)
+        self.assertEqual(sll.size(), 2)
+
+    def test_SLL_remove_mid(self):
+        sll = SLL()
+        sll.add_top(0)
+        sll.add_top(1)
+        sll.add_top(2)
+        node = sll.head.get_next().get_next()
+        sll.remove(node)
+        self.assertEqual(sll.head.get_data(), 2)
+        self.assertEqual(sll.head.get_next().get_data(), 1)
+        self.assertEqual(sll.size(), 2)
 
 if __name__ == '__main__':
     unittest.main()
