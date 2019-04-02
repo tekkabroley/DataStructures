49a9d3c5c542523da0c3946298482ca3e79cbe6f
Alex Broley <alexbroley@fb.com>
2019-04-01 19:19:10 -0700

    added double linked list class to dll.py

--- dll/dll.py
+++ dll/dll.py
@@ -34,3 +34,72 @@ class DLLNode(object):
 
     def set_prev(self, node):
         self.prev = node
+
+
+"""
+Double linked list attributes and methods
+head
+add_top
+add_bot
+remove
+contains
+size
+"""
+
+class DLL(object):
+    def __init__(self):
+        """ initialize empty double linked list """
+        self.head = None
+
+    def add_top(self, node):
+        """ add node to top of list """
+        curr = self.head
+        self.head = node
+        if curr:
+            node.set_next(curr)
+            curr.set_prev(node)
+
+    def add_bot(self, node):
+        """ add node to bottom of list """
+        curr = self.head
+        prev = None
+        while curr:
+            prev = curr
+            curr = curr.get_next()
+        if prev:
+            prev.set_next(node)
+            node.set_prev(prev)
+        else:
+            self.head = node
+
+    def size(self):
+        cnt = 0
+        curr = self.head
+        while curr:
+            cnt += 1
+            curr = curr.get_next()
+        return cnt
+
+    def remove(self, node):
+        prev = node.get_prev()
+        next = node.get_next()
+        if prev and next:
+            prev.set_next(next)
+            next.set_prev(prev)
+        elif prev:
+            prev.set_next(None)
+        elif next:
+            self.head = next
+        else:
+            self.head = None
+
+    def contains(self, data):
+        found = False
+        curr = self.head
+        while curr and not found:
+            value = curr.get_data()
+            if value == data:
+                found = True
+            else:
+                curr = curr.get_next()
+        return found
